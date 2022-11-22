01b737f05f661c4717f567b190bcfd81fa4f0dd9
Developer <Developer@dev.com>
2022-11-22 14:06:26 +0530

    commit..

--- SPRING BOOT/lw1-efictishop-springboot-rest-api-L1/src/main/java/com/demo/controller/EmployeeController.java
+++ SPRING BOOT/lw1-efictishop-springboot-rest-api-L1/src/main/java/com/demo/controller/EmployeeController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 @RestController
@@ -22,23 +24,58 @@ public class EmployeeController {
         return  htmlOutput;
     }
 
-//    JAVA OBJECT SERIALIZED INTO JSON
+    //    JAVA OBJECT SERIALIZED INTO JSON
     @GetMapping("/employee")
-    public EmployeeModel getEmployee(){
+    public EmployeeModel getEmployeess(){
         EmployeeModel employeeModel = new EmployeeModel(1,"Ben",5000);
         return employeeModel;
     }
-//    IP : RETURN A LIST OF 3-5 EMPLOYEES
+    //    IP : RETURN A LIST OF 3-5 EMPLOYEES
+    @GetMapping("/employees")
     public List getEmployeeList() {
-        return null;
+
+        List <EmployeeModel> employeeModelList = new ArrayList<>();
+        employeeModelList.add(new EmployeeModel(1,"Ben",5000));
+        employeeModelList.add(new EmployeeModel(2,"Charles",6000));
+        employeeModelList.add(new EmployeeModel(3,"Amber",7000));
+
+        return employeeModelList;
     }
     //    IP : RETURN A MAP OF 3-5 EMPLOYEES (EMPLOYEEID : KEY, EMPLOYEE INSTANCE : VALUE
+    @GetMapping("/employees-map")
     public Map getEmployeeMap() {
-        return null;
+
+        Map<Integer,EmployeeModel> map=new HashMap();
+        //Adding elements to map
+        map.put(1,new EmployeeModel(1,"Ben",5000));
+        map.put(2,new EmployeeModel(1,"Ben",5000));
+        map.put(3,new EmployeeModel(1,"Ben",5000));
+
+        return map;
     }
+
+
     //   EMPLOYEES OF DEPARTMENTS IT, FINANCE
     //   LIST OF TWO MAPS (EACH MAP WILL CONTAIN THREE EMPLOYEES OF THE DEPARTMENT)
-    public List<Map> getDepartmentWiseEmployees() {
-        return null;
+    @GetMapping("/employees-list-map")
+    public List<Map<Integer,EmployeeModel>> getDepartmentWiseEmployees() {
+
+        Map<Integer,EmployeeModel> map=new HashMap();
+        //Adding elements to map
+        map.put(1,new EmployeeModel(1,"IT-Ben",5000));
+        map.put(2,new EmployeeModel(2,"IT-Albert",5000));
+        map.put(3,new EmployeeModel(3,"IT-Sin",5000));
+
+        Map<Integer,EmployeeModel> map2=new HashMap();
+        //Adding elements to map
+        map2.put(4,new EmployeeModel(4,"Finance-Benneth",5000));
+        map2.put(5,new EmployeeModel(5,"Finance-Ken",6000));
+        map2.put(6,new EmployeeModel(6,"Finance-Ahmad",7000));
+
+        List<Map<Integer,EmployeeModel>> listMap = new ArrayList<>();
+        listMap.add(map);
+        listMap.add(map2);
+
+        return listMap;
     }
 }
