05ae6f817b019a3537540394c882c07357d2a107
Developer <Developer@dev.com>
2022-11-23 14:29:41 +0530

    commit..

--- SPRING BOOT/lw1-efictishop-springboot-rest-api-L1/src/main/java/com/demo/controller/EmployeeControllerDb.java
+++ SPRING BOOT/lw1-efictishop-springboot-rest-api-L1/src/main/java/com/demo/controller/EmployeeControllerDb.java
@@ -1,22 +1,35 @@
 package com.demo.controller;
+import com.demo.entity.EmployeeEntity;
 import com.demo.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeControllerDb {
-//  DEPENDENCY INJECTION
+    //  DEPENDENCY INJECTION
     @Autowired
     private EmployeeRepository employeeRepository;
     @GetMapping("/employees")
-    public List getEmployees(){
+    public List getEmployees() {
         System.out.println("GET ALL EMPLOYEES FROM REPOSITORY");
         List employeesList = null;
         employeesList = employeeRepository.findAll();
-        return  employeesList;
+        return employeesList;
+    }
+    @PostMapping("/employees")
+    public EmployeeEntity createEmployee(@RequestBody EmployeeEntity employeeFormClient) {
+        EmployeeEntity employeeEntity = employeeRepository.save(employeeFormClient);
+        return employeeEntity;
+    }
+    @DeleteMapping("/employees/{id}")
+    public EmployeeEntity deleteEmployeeById(@PathVariable(value = "id") Integer idFromClient) {
+//        FETCH THE EMPLOYEE ENTITY FROM THE DATABASE FOR THE SPECIFIC ID
+        Optional<EmployeeEntity> employeeEntityOptional = employeeRepository.findById(idFromClient);
+        EmployeeEntity deleteEmployee = employeeEntityOptional.get();
+        employeeRepository.delete(deleteEmployee);
+        return deleteEmployee;
     }
 }
